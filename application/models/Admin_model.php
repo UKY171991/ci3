65e3d7c7fb58c2e74e4cb7d3a7ede3b28d73cc3a
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2025-05-11 11:12:00 +0530

    Update Admin_model.php

--- application/models/Admin_model.php
+++ application/models/Admin_model.php
@@ -1,5 +1,10 @@
 <?php
 class Admin_model extends CI_Model {
+    public function __construct() {
+        parent::__construct();
+        $this->load->database();
+    }
+
     public function login($username, $password) {
         $this->db->where('username', $username);
         $this->db->where('password', md5($password));
